a65aad6e2ad04be77a7ca17193fbf10b2f9708c8
Emmanuel Palomeque Alcázar <epalomeque@gmail.com>
2016-10-08 14:56:55 -0500

    hasta subiendo archivos

--- carga_datos/models.py
+++ carga_datos/models.py
@@ -6,6 +6,10 @@ class archivo(models.Model):
     nombre_archivo = models.CharField(max_length=100)
     ubicacion_archivo = models.FileField(upload_to='documents/%Y/%m/%d')
 
+    def __unicode__(self):  # __str__ en Python 3
+        return self.nombre_archivo
+
+
 class cat_programas(models.Model):
     cv_prog = models.CharField (max_length=10, verbose_name='Clave del Programa')
     nombre_prog = models.CharField(max_length=80, verbose_name='Nombre del Programa')
